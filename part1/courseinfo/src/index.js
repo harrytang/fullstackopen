d1fce8b434a5667ee0ca21d8416fbc3628b65aea
Harry Tang <harry@powerkernel.com>
2019-11-23 15:24:48 +0200

    Exercise 1.1

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,58 @@
+/**
+ * @author Harry Tang <harry@powerkernel.com>
+ * @link https://powerkernel.com
+ * @copyright Copyright (c) 2019 Power Kernel
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* Header */
+const Header = (props) => {
+    return (<h1>{props.course}</h1>);
+};
+
+/* Content */
+const Content = (props) => {
+    return (
+        <>
+            <p>
+                {props.part1} {props.exercises1}
+            </p>
+            <p>
+                {props.part2} {props.exercises2}
+            </p>
+            <p>
+                {props.part3} {props.exercises3}
+            </p>
+        </>
+    );
+};
+
+/* Total */
+const Total = (props) => {
+    return (<p>Number of exercises {props.total}</p>);
+};
+
+/* Main App */
+const App = () => {
+    const course = 'Half Stack application development';
+    const part1 = 'Fundamentals of React';
+    const exercises1 = 10;
+    const part2 = 'Using props to pass data';
+    const exercises2 = 7;
+    const part3 = 'State of a component';
+    const exercises3 = 14;
+
+    return (
+        <div>
+            <Header course={course}/>
+            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2}
+                     exercises3={exercises3}/>
+            <Total total={exercises1 + exercises2 + exercises3}/>
+
+        </div>
+    )
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
