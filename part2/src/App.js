f47177fbe905e4ec2bb20ef9531e6ae1936a143f
Harry Tang <harry@powerkernel.com>
2019-11-25 18:17:18 +0200

    part2-8

--- part2/src/App.js
+++ part2/src/App.js
@@ -8,10 +8,14 @@ import React, {useState} from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '04412345678'}
+        {name: 'Arto Hellas', number: '040-123456'},
+        {name: 'Ada Lovelace', number: '39-44-5323523'},
+        {name: 'Dan Abramov', number: '12-43-234345'},
+        {name: 'Mary Poppendieck', number: '39-23-6423122'}
     ]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
+    const [search, setSearch] = useState('');
 
 
     const handlerNameChange = (e) => {
@@ -22,12 +26,15 @@ const App = () => {
         setNewNumber(e.target.value);
     };
 
+    const handlerSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
     const addPerson = (e) => {
         e.preventDefault();
-        if(persons.find(person=> person.name===newName)){
+        if (persons.find(person => person.name === newName)) {
             alert(`${newName} is already exist!`);
-        }
-        else {
+        } else {
             setPersons(persons.concat({name: newName, number: newNumber}));
             setNewName('');
             setNewNumber('');
@@ -36,23 +43,34 @@ const App = () => {
 
     };
 
+    // search
+    const records = search ===''
+        ? persons
+        : persons.filter(person=>person.name.search(new RegExp(search,"i"))>-1);
 
     return (
         <div>
-            <h2>Phonebook</h2>
+            <h1>Phonebook</h1>
+
+            <div>
+                search name: <input value={search} onChange={handlerSearch}/>
+            </div>
+
+            <h2>Add new</h2>
+
             <form onSubmit={addPerson}>
                 <div>
                     name: <input value={newName} onChange={handlerNameChange}/>
                 </div>
                 <div>
-                    number: <input value={newNumber} onChange={handlerNumberChange} />
+                    number: <input value={newNumber} onChange={handlerNumberChange}/>
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map((person, i) => <div key={i}>{person.name} {person.number}</div>)}
+            {records.map((person, i) => <div key={i}>{person.name} {person.number}</div>)}
         </div>
     )
 };
