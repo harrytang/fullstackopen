878b9701c485e465b9e2bcd7a1741b456e948bf0
Harry Tang <harry@powerkernel.com>
2019-11-25 17:18:18 +0200

    part2-6

--- part2/src/App.js
+++ part2/src/App.js
@@ -4,67 +4,40 @@
  * @copyright Copyright (c) 2019 Power Kernel
  */
 
-import React, {useState} from 'react';
-import Note from './components/Note';
+import React, {useState} from 'react'
 
-const App = (props) => {
-    const [notes, setNotes] = useState(props.notes);
-    const [newNote, setNewNote] = useState(
-        'a new note...'
-    );
-    const [showAll, setShowAll] = useState(true);
+const App = () => {
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas'}
+    ]);
+    const [newName, setNewName] = useState('');
 
-    const notesToShow = showAll
-        ? notes
-        : notes.filter(note => note.important === true);
-
-    const rows = () => notesToShow.map(note =>
-        <Note
-            key={note.id}
-            note={note}
-        />
-    );
-
-    const handleNoteChange = (event) => {
-        console.log(event.target.value);
-        setNewNote(event.target.value);
+    const handlerNameChange = (e) => {
+        setNewName(e.target.value);
     };
 
-    const addNote = (event) => {
-        event.preventDefault();
-        const noteObject = {
-            content: newNote,
-            date: new Date().toISOString(),
-            important: Math.random() > 0.5,
-            id: notes.length + 1,
-        };
-
-        setNotes(notes.concat(noteObject));
-        setNewNote('');
+    const addPerson = (e) => {
+        e.preventDefault();
+        setPersons(persons.concat({name: newName}));
+        setNewName('');
     };
-
 
 
     return (
         <div>
-            <h1>Notes</h1>
-
-            <div>
-                <button onClick={() => setShowAll(!showAll)}>
-                    show {showAll ? 'important' : 'all' }
-                </button>
-            </div>
-
-            <ul>
-                {rows()}
-            </ul>
-
-            <form onSubmit={addNote}>
-                <input value={newNote} onChange={handleNoteChange}/>
-                <button type="submit">save</button>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name: <input value={newName} onChange={handlerNameChange}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
             </form>
+            <h2>Numbers</h2>
+            {persons.map((person, i) => <div key={i}>{person.name}</div>)}
         </div>
     )
 };
 
-export default App;
+export default App
