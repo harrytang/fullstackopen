f66fff4ec7dfc45588021279cee7865c3a3e1e20
Harry Tang <harry@powerkernel.com>
2019-11-26 23:02:01 +0200

    part2-16

--- part2/src/App.js
+++ part2/src/App.js
@@ -5,7 +5,6 @@
  */
 
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
@@ -19,11 +18,11 @@ const App = () => {
 
     // load person from server
     useEffect(() => {
-        axios
-            .get('http://localhost:3001/persons')
-            .then(response => {
-                setPersons(response.data)
-            })
+        personService.getAll().then(
+            data=> {
+                setPersons(data)
+            }
+        );
     }, []);
 
 
