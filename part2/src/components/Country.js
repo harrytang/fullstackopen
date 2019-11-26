fde2bcbf77accf52dae8101130c32895084ac16f
Harry Tang <harry@powerkernel.com>
2019-11-26 22:05:22 +0200

    part2-14

--- part2/src/components/Country.js
+++ part2/src/components/Country.js
@@ -4,10 +4,27 @@
  * @copyright Copyright (c) 2019 Power Kernel
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const Country = ({country}) => {
+
+    const [weather, setWeather] = useState({});
+
+    useEffect(() => {
+        const url = `http://api.weatherstack.com/current?access_key=YOUR_API_KEY&query=${country.capital}`;
+        axios
+            .get(url)
+            .then(response => {
+                setWeather(response.data.current);
+            })
+    }, []);
+
+
+
     return (
+
+
         <div>
             <h2>{country.name}</h2>
             <div>capital {country.capital}</div>
@@ -23,6 +40,11 @@ const Country = ({country}) => {
                 }
             </ul>
             <div><img src={country.flag} alt={country.name} width="150"/></div>
+            <h2>weather in {country.capital}</h2>
+            <div>temperature: {weather.temperature} Celsius</div>
+            <div><img src={weather.weather_icons} width="80" alt={weather.weather_descriptions}/></div>
+            <div>wind: {weather.wind_speed} kph direction {weather.wind_dir}</div>
+
         </div>
     );
 };
