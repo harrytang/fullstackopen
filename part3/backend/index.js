ccd3b323a7444092965fd430ff07c508f44f29d9
Harry Tang <harry@powerkernel.com>
2019-11-27 23:02:50 +0200

    part3-2

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -29,6 +29,10 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 });
 
+app.get('/info', (req, res) => {
+    res.send(`phonbook has ${persons.length} people.<br/> ${new Date()}`)
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
