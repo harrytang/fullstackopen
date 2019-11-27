d21068ecb0818edeb170cf9f19e729a1074bf33b
Harry Tang <harry@powerkernel.com>
2019-11-27 22:58:47 +0200

    part3-1

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -7,83 +7,27 @@
 const express = require('express');
 const app = express();
 
-let notes = [
+let persons = [
     {
-        id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
     },
     {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
     },
     {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
     }
 ];
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 });
-
-app.get('/notes', (req, res) => {
-    res.json(notes)
-});
-
-app.get('/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => note.id === id);
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
-
-});
-
-app.delete('/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-    response.status(204).end()
-});
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0;
-    return maxId + 1
-};
-
-app.post('/notes', (request, response) => {
-    const body = request.body;
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    };
-
-    notes = notes.concat(note);
-
-    response.json(note);
-});
-
-
-
-console.log(...notes.map(n => n.id)    );
 
 const PORT = 3001;
 app.listen(PORT, () => {
