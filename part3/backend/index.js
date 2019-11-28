9a20637b50f19850e3c293bc280305f7325ab465
Harry Tang <harry@powerkernel.com>
2019-11-28 19:48:53 +0200

    part3-7

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -6,8 +6,10 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('tiny'));
 app.use(bodyParser.json());
 
 let persons = [
