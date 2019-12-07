fd0ac2406eef73097ca774d6a72854321d1bf630
Harry Tang <harry@powerkernel.com>
2019-12-07 12:31:42 +0200

    token_authentication

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const bodyParser = require('body-parser');
 const config = require('./utils/config');
 const notesRouter = require('./controllers/notes');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 
 const middleware = require('./utils/middleware');
@@ -22,7 +23,12 @@ const app = express();
 logger.info('connecting to', config.MONGODB_URI);
 
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
     .then(() => {
         logger.info('connected to MongoDB');
     })
@@ -37,6 +43,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
