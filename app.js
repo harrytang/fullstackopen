ef9d7624d663fb36f8078343e57a28cc1bea5ee6
Harry Tang <harry@powerkernel.com>
2019-12-07 14:09:14 +0200

    4.21

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const bodyParser = require('body-parser');
 const config = require('./utils/config');
 const blogRouter = require('./controllers/blog');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 
 
@@ -35,10 +36,12 @@ mongoose.connect(config.MONGODB_URI, {
 app.use(cors());
 app.use(express.static('build'));
 app.use(bodyParser.json());
+app.use(middleware.tokenExtractor);
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- controllers/blog.js
+++ controllers/blog.js
@@ -5,6 +5,9 @@
  */
 
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+
+
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
@@ -38,18 +41,24 @@ router.get('/:id', async (request, response, next) => {
 router.post('/', async (request, response, next) => {
     const body = request.body;
 
-    // find some user
-    const user = await User.findOne();
-
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes,
-        user: user._id
-    });
-
     try {
+        const token = request.token;
+        const decodedToken = token?jwt.verify(token, process.env.SECRET):null;
+
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' });
+        }
+
+        const user = await User.findById(decodedToken.id);
+
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes,
+            user: user._id
+        });
+
         const savedBlog = await blog.save();
         // update user
         user.blogs=user.blogs.concat(savedBlog._id);
@@ -65,8 +74,21 @@ router.post('/', async (request, response, next) => {
  */
 router.delete('/:id', async (request, response, next) => {
     try {
-        await Blog.findByIdAndRemove(request.params.id);
-        response.status(204).end();
+        const token = request.token;
+        const decodedToken = token?jwt.verify(token, process.env.SECRET):null;
+
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' });
+        }
+
+        const blog = await Blog.findOne({ _id: request.params.id, user: decodedToken.id });
+        if (blog) {
+            await blog.delete();
+            response.status(204).end();
+        } else {
+            response.status(404).end();
+        }
+
     } catch (e) {
         next(e);
     }
